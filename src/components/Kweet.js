7b2085ac4ca830e3381e05cba779215f739e3cec
Kwon770 <sckwon770@gmail.com>
2020-09-10 10:34:17 +0900

    3.5 Delete and Update  part Two

--- src/components/Kweet.js
+++ src/components/Kweet.js
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fbase";
 
-const Kweet = ({ kweetObj, isOwner }) => (
-  <div>
-    <h4>{kweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete</button>
-        <button>Edit</button>
-      </>
-    )}
-  </div>
-);
+const Kweet = ({ kweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newKweet, setNewKweet] = useState(kweetObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this kweet");
+    if (ok) {
+      await dbService.doc(`kweets/${kweetObj.id}`).delete();
+    }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.doc(`kweets/${kweetObj.id}`).update({
+      text: newKweet,
+    });
+    setEditing(false);
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewKweet(value);
+  };
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your kweet"
+              onChange={onChange}
+              value={newKweet}
+              required
+            />
+            <input type="submit" value="Update" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{kweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete</button>
+              <button onClick={toggleEditing}>Edit</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Kweet;
