573d72777cb6a1be0cc4619df02c091345bc7340
Kwon770 <sckwon770@gmail.com>
2020-09-21 19:51:59 +0900

    Updates

---
+++ precache-manifest.68fd4ff9c5c2645a099e16e297e4822d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ca6dfbbf8feb3680d359dfd2e17a3a",
+    "url": "/kwitter/index.html"
+  },
+  {
+    "revision": "28dce725815415f199cb",
+    "url": "/kwitter/static/js/2.f3eb2d6d.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/kwitter/static/js/2.f3eb2d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5a2f13f9dd8242c7b9",
+    "url": "/kwitter/static/js/main.1318b2b8.chunk.js"
+  },
+  {
+    "revision": "6050947a6f190eca50a3",
+    "url": "/kwitter/static/js/runtime-main.df10ca4b.js"
+  }
+]);
